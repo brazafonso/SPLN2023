8f86a50e80697f6f8fa59812fba2e48860db3019
brazafonso <61985783+brazafonso@users.noreply.github.com>
2023-03-23 12:40:34 +0000

    tokenizer usando argparse

--- TP5/tokenizerspln/tokenizerspln/__init__.py
+++ TP5/tokenizerspln/tokenizerspln/__init__.py
@@ -2,11 +2,9 @@
 """Module to tokenize books
 """
 
-__version__ = '0.3'
+__version__ = '0.4'
 
 import re
-import sys
-import fileinput
 from .utils import *
 
 list_poemas = []
@@ -28,35 +26,35 @@ def load_abrev():
         abrev_dic[ln] = abrevs
     return abrev_dic
 
-def treat_page_break(procedure_dic,text):
+def treat_page_break(args,text):
     """If flagged or default removes page breaks"""
     regex_nl = r'([a-z.0-9,;\-–?!\(\)])(\n\n)+([a-zA-Z.0-9,;\-–?!\(\)])'
     return re.sub(regex_nl,r'\1\n\3',text)
 
-def treat_punctuation(procedure_dic,text):
+def treat_punctuation(args,text):
     """If flagged or default separates punctuation from words"""
     regex_punc = r'([.!,?;:' + r'\"\-\”\–\`()\[\]])?(\w+)([.!,?;:' + r'\"\-\”\–\`()\[\]])'
     return re.sub(regex_punc,r'\1 \2 \3',text)
 
 
-def treat_chapters(procedure_dic,text):
+def treat_chapters(args,text):
     """If flagged or default tags chapters with the '#' delimeter\n
        Further subtitles or descriptions of the chapter will be delimeted between '[' and ']'
     """
     regex_cap = r'.*(CAPÍTULO +(\w+|\d+)).*\n((.+\n)*)'
     return re.sub(regex_cap,r'#\1\n[\3]\n',text)
 
-def treat_paragraphs(procedure_dic,text):
+def treat_paragraphs(args,text):
     """If flagged or default separates paragraphs from simple sentences with the '/PAR/' delimiter"""
     regex_par = r'([.!?;])\n(([^.!?;]|[\u00C0-\u017F]))'
     return re.sub(regex_par,r'\1\n/PAR/ \2',text)
 
-def treat_sentences_single_line(procedure_dic,text):
+def treat_sentences_single_line(args,text):
     """If flagged or default unifies sentences into a single line"""
     regex_sen = r'([^.!?])\n+([^.!?])'
     return re.sub(regex_sen,r'\1 \2',text)
 
-def treat_sentences_per_line(procedure_dic,text):
+def treat_sentences_per_line(args,text):
     """If flagged or default seperates every sentence in differente lines"""
     regex_sen = r'([^.!?][.?!])(\s|[^.])'
     text = re.sub(regex_sen,r'\1\2\n',text)
@@ -66,66 +64,58 @@ def treat_sentences_per_line(procedure_dic,text):
     return re.sub(regex_abrv,r'\1.\4',text)
 
 
-
-
 def save_poems(poema):
     """Saves a poem in a data structure"""
     list_poemas.append(poema[1])
     return f">>{len(list_poemas)}<<"
 
-def treat_poems(procedure_dic,text):
+def treat_poems(args,text):
     """If flagged removes poems from text and saves it on data structure\n
        Poems need to be marked between <poem> and </poem>
     """
-    if procedure_dic['poems']:
+    if args.poem:
         regex_poema = r'<poem>(.*?)</poem>'
         text = re.sub(regex_poema,save_poems,text,flags=re.S)
         print(list_poemas)
     return text
 
 def tokenizer():
-    print(f"""
-    --------------------------------------------------------------------
-                    **Tokenizer SPLN version {__version__}**
-    --------------------------------------------------------------------
-    """)
-    procedure_dic = process_arguments()
-    if not have_errors():
-        text = get_input()
-        
-        # 0. Quebra de pagina
-        # 1. Separar pontuação das palavras
-        # 2. Marcar capitulos
-            #* titulo do capitulo na linha seguinte 
-            #* keywords para procurar por capitulo (ex.: multilingua)
-        # 3. Separar paragrafos de linhas pequenas
-        # 4. Juntar linhas da mesma frase
-        # 5. Uma frase por linha
-        # 6. Tratar Poemas (tagged)
+    args = process_arguments(__version__)
+    #print(args)
+    #procedure_dic = process_arguments()
+    text = get_input(args)
+    
+    # 0. Quebra de pagina
+    # 1. Separar pontuação das palavras
+    # 2. Marcar capitulos
+        #* titulo do capitulo na linha seguinte 
+        #* keywords para procurar por capitulo (ex.: multilingua)
+    # 3. Separar paragrafos de linhas pequenas
+    # 4. Juntar linhas da mesma frase
+    # 5. Uma frase por linha
+    # 6. Tratar Poemas (tagged)
 
 
-        # 6. Tratar Poemas (tagged)
-        text = treat_poems(procedure_dic,text)
+    # 6. Tratar Poemas (tagged)
+    text = treat_poems(args,text)
 
-        # 2. Marcar capitulos
-        text = treat_chapters(procedure_dic,text)
+    # 2. Marcar capitulos
+    text = treat_chapters(args,text)
 
-        # 0. Quebra de pagina
-        text = treat_page_break(procedure_dic,text)
-        
-        # 3. Separar paragrafos de linhas pequenas
-        text = treat_paragraphs(procedure_dic,text)
+    # 0. Quebra de pagina
+    text = treat_page_break(args,text)
+    
+    # 3. Separar paragrafos de linhas pequenas
+    text = treat_paragraphs(args,text)
 
-        # 4. Juntar linhas da mesma frase
-        text = treat_sentences_single_line(procedure_dic,text)
+    # 4. Juntar linhas da mesma frase
+    text = treat_sentences_single_line(args,text)
 
-        # 5. Uma frase por linha
-        text = treat_sentences_per_line(procedure_dic,text)
+    # 5. Uma frase por linha
+    text = treat_sentences_per_line(args,text)
 
-        # 1. Separar pontuação das palavras
-        text = treat_punctuation(procedure_dic,text)
+    # 1. Separar pontuação das palavras
+    text = treat_punctuation(args,text)
 
-        write_output(text)
-
-    print_errors()
+    write_output(args,text)
 
--- TP5/tokenizerspln/tokenizerspln/utils.py
+++ TP5/tokenizerspln/tokenizerspln/utils.py
@@ -1,106 +1,38 @@
 import sys
-import re
+import argparse
 
-flags_list = ['p','o']
-procedure_dic = {
-    'input' : 'stdin',
-    'output' : 'stdout',
-    'poems' : False,
-}
-errors = []
-
-def get_input():
+def get_input(args):
     """Gets the input text according with the register in procedure_dic"""
     text = ''
-    if procedure_dic['input'] == 'stdin':
+    if not args.input:
         for line in sys.stdin:
             text+=line
     else:
-        input = open(procedure_dic['input'],'r')
+        input = args.input
         text = input.read()
     return text
 
-def write_output(output):
+def write_output(args,output):
     """Writes the ouput on the location registered in procedure_dic"""
-    if procedure_dic['output'] == 'stdout':
+    if not args.output:
         sys.stdout.write(output)
     else:
-        file = open(procedure_dic['output'],'w')
+        file = args.output
         file.write(output)
 
 
-def register_error(error):
-    """Register an error on the error stack"""
-    errors.append(f'Error: {error}')
-
-def have_errors():
-    """Reports if there have been errors in the program"""
-    return len(errors) > 0
-
-def print_errors():
-    """Imprime todos os erros detetados"""
-    for error in errors:
-        print(error)
-
-def update_files(arg,field):
-    """Atualiza os ficheiros registados para leitura ou escrita pelo programa"""
-    files = arg.split()
-    if len(files) == 1:
-        if (field == 'input' and procedure_dic[field] != 'stdin') or field == 'output' and procedure_dic[field] != 'stdout':
-            register_error(f'Multiple {field} files given (4) - {files}')
-        else:
-            procedure_dic[field] = files[0]
-    elif len(files) > 1:
-        register_error(f'Multiple input files given (3)')
-
-
-def get_first_word(arg):
-    """Return flag from an argument (first word of the string)"""
-    return re.sub(r'((\w|\.|_)+)\s*.*',r'\1',arg)
-
-def duplicate_flags(args):
-    """Checks for duplicate flags on initial arguments"""
-    n_flags = {}
-    for flag in flags_list:
-        n_flags[flag] = 0
-
-    for arg in args:
-        word = get_first_word(arg)
-        if word in flags_list:
-            n_flags[word] += 1
-
-    duplicate = False
-    for flag,n in n_flags.items():
-        if n > 1:
-            duplicate = True
-            register_error(f"Multiple {flag} used (2)")
-    return duplicate
-
-def process_flag(flag,arg):
-    """Process a flag, updating procedure_dic accordingly"""
-    rest = arg[len(flag):]
-    if flag == 'p':
-        procedure_dic['poems'] = True
-        update_files(rest,'input')
-    elif flag == 'o':
-        update_files(rest,'output')
-
-
-def process_arguments():
-    """Processes and checks for errors on the initial arguments"""
-    args = " ".join(sys.argv[1:])
-    if '-' in args:
-        args = args.split('-')
-        if not duplicate_flags(args):
-            for arg in args:
-                word = get_first_word(arg)
-                if word in flags_list:
-                    process_flag(word,arg)
-                else:
-                    if len(arg.split()) > 1:
-                        register_error(f'Multiple input files given (1) - {arg}')
-                    else:
-                        procedure_dic['input'] = word
-    else:
-        update_files(args,'input')
-    return procedure_dic
+def process_arguments(__version__):
+    parser = argparse.ArgumentParser(
+        prog='tok',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=f'''
+    --------------------------------------------------------------------
+                    **Tokenizer SPLN version {__version__}**
+    --------------------------------------------------------------------
+                        Module to tokenize books'''
+    )
+    parser.add_argument('input',metavar='filename',type=argparse.FileType('r'),nargs='?',help='input file of the text to tokenize',default=None)
+    parser.add_argument('-o','--output',help='defines an output file',type=argparse.FileType('w'), nargs='?',default=None)
+    parser.add_argument('-p','--poem',help='saves poems delimited inside <poem></poem> in the text',action='store_true',default=False)
+    parser.add_argument('--version','-V', action='version', version='%(prog)s '+__version__)
+    return parser.parse_args()
