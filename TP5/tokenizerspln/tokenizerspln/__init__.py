d7ffe602022a07a4e555a91d654ce3a797779af6
brazafonso <61985783+brazafonso@users.noreply.github.com>
2023-03-23 20:40:19 +0000

    opcao de lingua e funcionalidades
    add:
    - opcao de lingua
     * impacto nas abreviaturas
     * impacto nos capitulos
    -opcoes de funcionalidades

--- TP5/tokenizerspln/tokenizerspln/__init__.py
+++ TP5/tokenizerspln/tokenizerspln/__init__.py
@@ -15,94 +15,115 @@ def remove_empty(l):
     new_list = [x.strip() for x in l if x.strip()]
     return new_list
     
+def make_regex_lan(args,dict):
+    """
+    Creates regex expression to catch elments from the given languages dict in the same group\n
+    """
+    regex = r'('
+    one_abrev = False
+    groups = 1
+    for lan in dict:
+        for i,abr in enumerate(dict[lan]):
+            one_abrev = True
+            regex += r'(' + abr + r')'
+            groups += 1
+            if i != len(dict[lan])-1:
+                regex += r'|'
+    
+    regex += r')'
+    return (regex,groups) if one_abrev else (None,groups)
 
-
+def load_translations(args,file_path):
+    """
+    Loads translations from the config file, only gathers the translations of the defined language\n
+    English by default
+    """
+    file = open(f'{path}{file_path}','r')
+    txt = file.read()
+    file.close()
+    ln_list = txt.split('#')
+    ln_list = remove_empty(ln_list)
+    dic = {}
+    for elem in ln_list:
+        ln,*translations = remove_empty(elem.split('\n'))
+        if args.language[0] in ln:
+            dic[ln] = translations
+            break
+    return dic
 
 def treat_page_break(args,text):
     """If flagged or default removes page breaks"""
-    regex_nl = r'([a-z.0-9,;\-–?!\(\)])(\n\n)+([a-zA-Z.0-9,;\-–?!\(\)])'
-    return re.sub(regex_nl,r'\1\n\3',text)
+    if is_default() or '-pb' in args.options:
+        regex_nl = r'([a-z.0-9,;\-–?!\(\)])(\n\n)+([a-zA-Z.0-9,;\-–?!\(\)])'
+        text = re.sub(regex_nl,r'\1\n\3',text)
+    return text
 
 def treat_punctuation(args,text):
     """If flagged or default separates punctuation from words"""
-    regex_punc = r'([.!,?;:' + r'\"\-\”\–\`()\[\]])?(\w+)([.!,?;:' + r'\"\”\–\`()\[\]])'
-    text = re.sub(regex_punc,r'\1 \2 \3',text)
+    if is_default() or '-pu' in args.options:
+        regex_punc = r'([.!,?;:' + r'\"\-\”\–\`()\[\]])?(\w+)([.!,?;:' + r'\"\”\–\`()\[\]])'
+        text = re.sub(regex_punc,r'\1 \2 \3',text)
 
-    """Fixes abreviatures, default recognizes english"""
-    abrevs = load_abrev()
-    regex_abrv,groups = make_regex_abrevs(args,abrevs)
-    if regex_abrv:
-        regex_abrv += r'\s*\.'
-        text = re.sub(regex_abrv,r'\1.',text)
+        """Fixes abreviatures, default recognizes english"""
+        abrevs = load_translations(args,'/conf/abrev.txt')
+        regex_abrv,groups = make_regex_lan(args,abrevs)
+        if regex_abrv:
+            regex_abrv += r'\s*\.'
+            text = re.sub(regex_abrv,r'\1.',text)
     return text 
+
 
 
 def treat_chapters(args,text):
     """If flagged or default tags chapters with the '#' delimeter\n
        Further subtitles or descriptions of the chapter will be delimeted between '[' and ']'
     """
-    regex_cap = r'.*(CAPÍTULO +(\w+|\d+)).*\n(.*)(\n\n)+'
-    return re.sub(regex_cap,r'#\1\n[\3]\n/PAR/',text)
-
-def treat_paragraphs(args,text):
-    """If flagged or default separates paragraphs from simple sentences with the '/PAR/' delimiter"""
-    regex_par = r'([.!?;])\n(([^.!?;]|[\u00C0-\u017F]))'
-    return re.sub(regex_par,r'\1\n/PAR/ \2',text)
-
-def treat_sentences_single_line(args,text):
-    """If flagged or default unifies sentences into a single line"""
-    regex_sen = r'([^.!?])\n+([^.!?])'
-    text = re.sub(regex_sen,r'\1 \2',text)
-    """Fix Chapters"""
-    regex_chap = r'(#CAPÍTULO +(\w+|\d+) \[.*\])'
-    text = re.sub(regex_chap,r'\1\n',text)
+    if is_default() or '-c' in args.options:
+        chapter_delims = load_translations(args,'/conf/chapter_delim.txt')
+        regex_caps,groups = make_regex_lan(args,chapter_delims)
+        regex_cap = r'.*'
+        regex_cap += regex_caps + r' +(\w+|\d+).*\n(.*)(\n\n)+'
+        text = re.sub(regex_cap,rf'#\1 \{groups+1}\n[\{groups+2}]\n/PAR/',text)
     return text
 
 
-def load_abrev():
-    """Loads abreviatures from the config file"""
-    file = open(f'{path}/conf/abrev.txt','r')
-    txt = file.read()
-    file.close()
-    ln_list = txt.split('#')
-    ln_list = remove_empty(ln_list)
-    abrev_dic = {}
-    for lan in ln_list:
-        ln,*abrevs = remove_empty(lan.split('\n'))
-        abrev_dic[ln] = abrevs
-    return abrev_dic
 
-def make_regex_abrevs(args,abrevs):
-    """Creates regex expression to catch abreviatures from the given dict in the same group'
-    By default will only use the english abreviatures
-    """
-    regex = r'('
-    one_abrev = False
-    groups = 1
-    for lan in abrevs:
-        for i,abr in enumerate(abrevs[lan]):
-            one_abrev = True
-            regex += r'(' + abr + r')'
-            groups += 1
-            if i != len(abrevs[lan])-1:
-                regex += r'|'
-    
-    regex += r')'
-    return (regex,groups) if one_abrev else (None,groups)
+def treat_paragraphs(args,text):
+    """If flagged or default separates paragraphs from simple sentences with the '/PAR/' delimiter"""
+    if is_default() or '-par' in args.options:
+        regex_par = r'([.!?;])\n(([^.!?;]|[\u00C0-\u017F]))'
+        text = re.sub(regex_par,r'\1\n/PAR/ \2',text)
+    return text
+
+
+
+
+def treat_sentences_single_line(args,text):
+    """If flagged or default unifies sentences into a single line"""
+    if is_default() or '-js' in args.options:
+        regex_sen = r'([^.!?])\n+([^.!?])'
+        text = re.sub(regex_sen,r'\1 \2',text)
+        """Fix Chapters"""
+        regex_chap = r'(#CAPÍTULO +(\w+|\d+) \[.*\])'
+        text = re.sub(regex_chap,r'\1\n',text)
+    return text
+
+
+
+
 
 def treat_sentence_per_line(args,text):
     """If flagged or default seperates every sentence in differente lines"""
-    regex_sen = r'([^.!?][.?!])( |[^.\n])'
-    text = re.sub(regex_sen,r'\1\2\n',text)
-    
-    """Fixes abreviatures, default recognizes english"""
-    abrevs = load_abrev()
-    regex_abrv,groups = make_regex_abrevs(args,abrevs)
-    #FIXME utilizar dicionario das abreviaturas para completar a expressao regular
-    #FIXME permitir opção de linguas para usar apenas um conjunto de abreviaturas
-    if regex_abrv:
-        regex_abrv += r'\.(\s*)\n'
-        text = re.sub(regex_abrv,rf'\1.\{groups+1}',text)
+    if is_default() or '-spl' in args.options:
+        regex_sen = r'([^.!?][.?!])( |[^.\n])'
+        text = re.sub(regex_sen,r'\1\2\n',text)
+        
+        """Fixes abreviatures, default recognizes english"""
+        abrevs = load_translations(args,'/conf/abrev.txt')
+        regex_abrv,groups = make_regex_lan(args,abrevs)
+        if regex_abrv:
+            regex_abrv += r'\.(\s*)\n'
+            text = re.sub(regex_abrv,rf'\1.\{groups+1}',text)
     return text
 
 
@@ -129,8 +150,6 @@ def tokenizer():
     # 0. Quebra de pagina
     # 1. Separar pontuação das palavras
     # 2. Marcar capitulos
-        #*FIXME titulo do capitulo na linha seguinte 
-        #*FIXME keywords para procurar por capitulo (ex.: multilingua)
     # 3. Separar paragrafos de linhas pequenas
     # 4. Juntar linhas da mesma frase
     # 5. Uma frase por linha
--- TP5/tokenizerspln/tokenizerspln/utils.py
+++ TP5/tokenizerspln/tokenizerspln/utils.py
@@ -1,6 +1,12 @@
 import sys
 import argparse
 
+
+default = True
+
+def is_default():
+    return default
+
 def get_input(args):
     """Gets the input text according with the register in procedure_dic"""
     text = ''
@@ -21,6 +27,18 @@ def write_output(args,output):
         file.write(output)
 
 
+class defaultAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        global default
+        default = False
+        if namespace.options:
+            namespace.options.append(option_string)
+        else:
+            namespace.options = [option_string]
+       
+
+
+
 def process_arguments(__version__):
     parser = argparse.ArgumentParser(
         prog='tok',
@@ -29,10 +47,21 @@ def process_arguments(__version__):
     --------------------------------------------------------------------
                     **Tokenizer SPLN version {__version__}**
     --------------------------------------------------------------------
-                        Module to tokenize books'''
+                Module to tokenize books (English by default)'''
     )
     parser.add_argument('input',metavar='filename',type=argparse.FileType('r'),nargs='?',help='input file of the text to tokenize',default=None)
     parser.add_argument('-o','--output',help='defines an output file',type=argparse.FileType('w'), nargs=1,default=None)
+    parser.add_argument('-lan','--language',help='''
+    defines a language to recognize, accordingly will use the languages settings (abreviatures, delimiters)
+        (default to english)                    
+                        ''',type=str, nargs=1,default='en')
     parser.add_argument('-p','--poem',help='saves poems delimited inside <poem></poem> in the text',action='store_true',default=False)
+    parser.add_argument('-c','--chapter',help='treat chapters using the language delimiters (default english)',dest='options',action=defaultAction,nargs='?')
+    parser.add_argument('-pu','--punctuation',help='treat punctuation using the language abreviations (default english)',dest='options',action=defaultAction,nargs=0)
+    parser.add_argument('-pb','--pagebreak',help='treat pagebreak',dest='options',action=defaultAction,nargs=0)
+    parser.add_argument('-js','--JoinSentence',help='treat sentences broken between lines, bringing them to the same line',dest='options',action=defaultAction,nargs=0)
+    parser.add_argument('-spl','--SentencePerLine',help='each sentence is seperated to an individual line',dest='options',action=defaultAction,nargs=0)
+    parser.add_argument('-par','--paragraphs',help='treat paragraphs, delimiting them with /PAR/',dest='options',action=defaultAction,nargs=0)
     parser.add_argument('--version','-V', action='version', version='%(prog)s '+__version__)
+
     return parser.parse_args()
