3f3104909766484dc40566921149218e97159bd4
brazafonso <61985783+brazafonso@users.noreply.github.com>
2023-04-02 15:46:37 +0100

    scraping da pagina de autor

--- TP1/bookscraper/__init__.py
+++ TP1/bookscraper/__init__.py
@@ -5,6 +5,7 @@
 __version__ = '0.0.2'
 
 import os
+from typing import Set
 import requests
 import re
 import json
@@ -13,7 +14,7 @@ import jellyfish
 import lxml.html as lh
 from .utils import *
 from .book import Book
-
+from .author import Author
 path = os.path.dirname(os.path.realpath(__file__))
 errors = []
 
@@ -24,13 +25,16 @@ TODO:
 		* Apanhar todas as reviews de uma pagina (manipulando o javascript inerente)
 	- Criar dataset com as reviews conseguidas
 	- Criar/permitir uso de modelo de aprendizagem profunda para analisar reviews
-
+	- Permitir uma opção em que é indicado um ficheiro estilo json para fazer um conjunto de pesquisas de uma só vez
 """
 
 
 def add_error(msg):
 	errors.append(f'Error: {msg}\n')
 
+
+def search_book_option(args):
+	return args.isbn or args.id or args.btitle
 
 def get_book_page(args)->requests.Response:
 	"""Performs a get request to goodreads for a book with a give isbn, work id or search name"""
@@ -116,7 +120,14 @@ def scrape_book_page(html_page)->Book:
 
 	book_publishing_date = re.sub(r'.*(\b\w+\b\s*\d+\s*,\s*\d+)',r'\1',page.find('p',{'data-testid':'publicationInfo'}).get_text()).strip()
 
-	return Book(book_name,book_isbn,book_author,book_description,book_publishing_date,book_score,book_nratings,book_nreviews,book_npages,book_language,book_genres)
+	return Book(book_name,book_isbn,book_author,
+	     book_description,book_publishing_date,book_score,
+		 book_nratings,book_nreviews,book_npages,
+		 book_language,book_genres)
+
+
+
+
 
 
 def get_author_page(args)->requests.Response:
@@ -125,7 +136,7 @@ def get_author_page(args)->requests.Response:
 	"""
 	r= None
 	# Check flag
-	if args.author:
+	if args.author and not search_book_option(args):
 		a = args.author.lower().replace(' ','')
 		match = re.match(r'\d+$',a)
 		# Search by author id
@@ -152,6 +163,7 @@ def get_author_page(args)->requests.Response:
 					similarityDic.append((name,url,difference))
 				if difference == 0:
 					break
+
 			if len(similarityDic)>0:
 				similarityDic = sorted(similarityDic,key=lambda x: x[2])
 				r = requests.get(similarityDic[0][1])
@@ -160,21 +172,87 @@ def get_author_page(args)->requests.Response:
 	return r
 
 
+def scrape_author_page(html_page:str)->Author:
+	"""Scrapes an author's page for info such as name, birthday, average score, number of reviews and others"""
+	page = BeautifulSoup(html_page,features='lxml')
+
+	author_name = page.find('h1',{'class':'authorName'}).get_text().strip()
+	author_birthdate = page.find('div',{'class':'dataItem','itemprop':"birthDate"}).get_text().strip()
+	author_birthplace = page.find('div',{'class':'dataTitle'}).next_sibling.get_text().strip()
+	author_deathdate = page.find('div',{'class':'dataItem','itemprop':"deathDate"}).get_text().strip()
+	author_website = page.find('a',{'itemprop':"url"}).get_text().strip()
+	author_description = page.find('div',{'class':'aboutAuthorInfo'}).find('span',{'style':'display:none'}).get_text().strip()
+	author_averageRating = page.find('span',{'class':'average','itemprop':'ratingValue'}).get_text().strip()
+	author_nratings = page.find('span',{'class':'value-title','itemprop':'ratingCount'}).get_text().strip()
+	author_nreviews = page.find('span',{'class':'value-title','itemprop':'reviewCount'}).get_text().strip()
+	author_nUniqueWorks = None
+
+	# Catch author's genres and influences
+	dataItems = page.find_all('div',{'class':'dataItem'})
+	author_genres = []
+	author_influences = []
+	works_url = None
+	for item in dataItems:
+		items = item.find_all('a')
+		for item in items:
+			if item['href']:
+				if re.search('genres',item['href']):
+					author_genres.append(item.get_text().strip())
+				elif re.search('/author/show/',item['href']):
+					author_influences.append(item.get_text().strip())
+
+	author_stats = page.find('div',{'class':'hreview-aggregate'})
+	items = author_stats.find_all('a')
+	for item in items:
+		if re.search('/author/list/',item['href']):
+			print(item)
+			author_nUniqueWorks = item.get_text().strip()
+			works_url = item['href']
+
+	author_works = get_author_works(works_url)
+
+	return Author(author_name,author_birthdate,author_birthplace,
+	       author_deathdate,author_website,author_genres,
+		   author_influences,author_description,author_averageRating,
+		   author_nratings,author_nreviews,author_nUniqueWorks,author_works)
+
+def get_author_works(works_url:str)->Set[str]:
+	"""Get the list of unique works of an author"""
+	# FIXME : needs to search through all the pages of works (only searching the first currently)
+	works = set()
+	print('found0',works_url)
+	if works_url:
+		r = requests.get(f'https://www.goodreads.com/{works_url}')
+		print('found1')
+		if r.status_code == 200:
+			print('found2')
+			page = BeautifulSoup(r.content.decode(r.encoding),features='lxml')
+			books = page.find_all('a',{'class':'bookTitle'})
+			for book in books:
+				print('found3')
+				works.add(book.find('span',{'itemprop':'name'}).get_text().strip())
+	
+	if works:
+		return works
+	else:
+		return None
+
+
 
 def work_in_progress(args):
 	"""Testing new implementations"""
-	r = get_author_page(args)
 
 	if not args.isbn:
 		args.isbn = "9781846144769" #teste
 
-	if r:
-		soup = BeautifulSoup(r.content.decode(r.encoding),features='lxml')
-		prettyHTML3 = soup.prettify()
+	# r = get_author_page(args)
+	# if r:
+	# 	soup = BeautifulSoup(r.content.decode(r.encoding),features='lxml')
+	# 	prettyHTML3 = soup.prettify()
 
-		file = open(f'{path}/test/teste3.html','w')
-		file.write(prettyHTML3)
-		file.close()
+	# 	file = open(f'{path}/test/teste3.html','w')
+	# 	file.write(prettyHTML3)
+	# 	file.close()
 
 	# r = get_book_page(args)
 	# if r
@@ -200,43 +278,31 @@ def work_in_progress(args):
 
 	# book = scrape_book_page(prettyHTML1)
 
-	# file = open(f'{path}/test/teste.html','r')
-	# r = file.read()
-	# file.close()
-	# book = scrape_book_page(r)
-	# utils.write_output(args,book.__str__())
+	file = open(f'{path}/test/teste3.html','r')
+	r = file.read()
+	file.close()
+	author = scrape_author_page(r)
+	utils.write_output(args,author.__str__(True))
 
 def bookscraper():
 	"""Main function of the program"""
+	# FIXME: Decidir resultados do programa (html ou resultados de scrape, no ultimo caso flags para informacao extra tipo descricao)
 	args = utils.process_arguments(__version__)
-	results = []
+	work_in_progress(args)
 
-	r = get_book_page(args)
-	if r:
-		results.append(utils.prettify_html(r))
+	# results = []
+
+	# # Get the page of a book
+	# r = get_book_page(args)
+	# if r:
+	# 	book = scrape_book_page(utils.prettify_html(r))
+	# 	results.append(book.__str__())
 	
-	r = get_author_page(args)
-	if r:
-		results.append(utils.prettify_html(r))
-	# book_id = re.sub(r'[^0-9]+(\d+).*',r'\1',r.url)
-	# print(book_id)
+	# # Get the page of an author
+	# r = get_author_page(args)
+	# if r:
+	# 	author = scrape_author_page(utils.prettify_html(r))
+	# 	results.append(author.__str__())
 
-	# r = get_book_reviews(book_id)
-
-	# if r
-		# soup = BeautifulSoup(r.content.decode(r.encoding),features='lxml')
-		# prettyHTML2 = soup.prettify()
-
-		# file = open(f'{path}/test/teste2.html','w')
-		# file.write(prettyHTML2)
-		# file.close()
-
-	# book = scrape_book_page(prettyHTML1)
-
-	# file = open(f'{path}/test/teste.html','r')
-	# r = file.read()
-	# file.close()
-	# book = scrape_book_page(r)
-	# utils.write_output(args,book.__str__())
-	utils.write_output(args,results)
-	utils.write_errors(args,errors)
+	# utils.write_output(args,results)
+	# utils.write_errors(args,errors)
--- TP1/bookscraper/book.py
+++ TP1/bookscraper/book.py
@@ -1,6 +1,11 @@
+from typing import Set
+
 class Book:
     """Class to represent and save data of a book"""
-    def __init__(self,name,isbn,author=None,description=None,publishing_date=None,score=None,nratings=None,nreviews=None,npages=None,language=None,genres=None):
+    def __init__(self,name:str,isbn:str,author:str=None,
+                 description:str=None,publishing_date:str=None,
+                 score:float=None,nratings:int=None,nreviews:int=None,
+                 npages:int=None,language:str=None,genres:Set[str]=None):
         self.name = name
         self.isbn = isbn
         self.author = author
@@ -13,69 +18,69 @@ class Book:
         self.language = language
         self.genres = genres
 
-    def name(self):
+    def name(self)->str:
         return self.name
     
-    def isbn(self):
+    def isbn(self)->str:
         return self.isbn
     
-    def author(self):
+    def author(self)->str:
         return self.author
     
-    def description(self):
+    def description(self)->str:
         return self.description
     
-    def score(self):
+    def score(self)->float:
         return self.score
     
-    def nratings(self):
+    def nratings(self)->int:
         return self.nratings
     
-    def nreviews(self):
+    def nreviews(self)->int:
         return self.nreviews
     
-    def npages(self):
+    def npages(self)->int:
         return self.npages
     
-    def language(self):
+    def language(self)->str:
         return self.language
     
-    def genres(self):
+    def genres(self)->Set[str]:
         return self.genres
     
-    def publishing_date(self):
+    def publishing_date(self)->str:
         return self.publishing_date
     
 
-    def set_author(self,author):
+    def set_author(self,author:str):
         self.author = author
     
-    def set_description(self,description):
+    def set_description(self,description:str):
         self.description=description
     
-    def set_score(self,score):
+    def set_score(self,score:float):
         self.score=score
     
-    def set_nratings(self,nratings):
+    def set_nratings(self,nratings:int):
         self.nratings=nratings
     
-    def set_nreviews(self,nreviews):
+    def set_nreviews(self,nreviews:int):
         self.nreviews=nreviews
     
-    def set_npages(self,npages):
+    def set_npages(self,npages:int):
         self.npages=npages
     
-    def set_language(self,language):
+    def set_language(self,language:str):
         self.language=language
     
-    def set_genres(self,genres):
+    def set_genres(self,genres:Set[str]):
         self.genres=genres
     
-    def set_publishing_date(self,publishing_date):
+    def set_publishing_date(self,publishing_date:str):
         self.publishing_date=publishing_date
 
 
-    def __str__(self,verbose=False):
+    def __str__(self,verbose=False)->str:
         text = f'''
           ________________________
         /
